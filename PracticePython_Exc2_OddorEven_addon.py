e3012c989e9eca3fafff20bf74bd8f1fad7ee7e0
cravingdata <32245139+cravingdata@users.noreply.github.com>
2017-11-18 12:12:30 -0500

    Add files via upload
    Including numbers that are multiples of 4, with 3 different print-out for multiples of 4, even, and odd.

---
+++ PracticePython_Exc2_OddorEven_addon.py
@@ -0,0 +1,7 @@
+number = float(raw_input("Give me a number: "))
+if number % 4 == 0:
+    print("Your number is a multiple of 4.")
+elif number % 2 == 0:
+    print("Your number is even.")
+else:
+    print("Your number is odd. Get it? :P")
